9f4b07b24cc5a456da9b0ec67944ce0960f01fba
Tetiana Yasko <taniayasko2015@gmail.com>
2021-08-02 15:26:25 +0300

    Updates

---
+++ precache-manifest.9d6c615816888677bff158a4ec67e10a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f64a2b75a07d1713ff3b10879c8ced0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b3bd9fc01a43d8fd563",
+    "url": "./static/css/2.1890a892.chunk.css"
+  },
+  {
+    "revision": "f55deee9c0ec4518d230",
+    "url": "./static/css/main.df7b5cb2.chunk.css"
+  },
+  {
+    "revision": "0b3bd9fc01a43d8fd563",
+    "url": "./static/js/2.6c923108.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6c923108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55deee9c0ec4518d230",
+    "url": "./static/js/main.5fa13985.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
